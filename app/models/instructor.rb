0eee916cccc3cae6d2abbec121f39ad9980d8b95
taniabelalov <belalov.tania16@gmail.com>
2019-09-02 16:08:34 -0400

    implamentation

--- app/models/instructor.rb
+++ app/models/instructor.rb
@@ -1,3 +1,40 @@
 class Instructor
 
+    attr_accessor :name
+
+    @@all = []
+
+    def initialize(name)
+        @name = name
+        @@all << self
+    end 
+
+    def self.all
+        @@all
+    end 
+
+    def pass_student(student, test_name)
+        #If there is a BoatingTest whose 
+        #name and student match the values passed in, this method should update the status of 
+        #that BoatingTest to 'passed'. If there is no matching test, this method should create a 
+        #test with the student, that boat test name, 
+        #and the status 'passed'. Either way, it should return the BoatingTest instance.
+        passed_test = BoatingTest.all.find {|boatingtest| boatingtest.student == student && boatingtest.test_name == test_name}
+        if passed_test
+            test_status = "passed"
+        else 
+            BoatingTest.new(student, test_name, test_status = "passed", self)
+        end 
+    end 
+   
+
+    def fail_student(student, test_name)
+        failed_test = BoatingTest.all.find {|boatingtest| boatingtest.student == student && boatingtest.test_name == test_name}
+        if failed_test
+            test_status = "failed"
+        else 
+            BoatingTest.new(student, test_name, test_status = "feiled", self)
+        end
+    end 
+
 end
--- app/models/student.rb
+++ app/models/student.rb
@@ -1,3 +1,37 @@
 class Student
 
+    attr_accessor :first_name
+
+    @@all = []
+
+    def initialize(first_name)
+        @first_name = first_name
+        @@all << self
+    end 
+
+    def add_boating_test
+        #should initialize a new boating test with a Student (Object), a boating test name (String), 
+        #a boating test status (String), and an Instructor (Object)
+        BoatingTest.new(self, test_name, test_status, instructor)
+    end
+
+    def grade_percentage
+        #return the percentage of tests that the student has passed, a Float (so if a student has 
+        #passed 3 / 9 tests that they've taken, this method should return the Float 33.33)
+        total_tests = BoatingTest.all.map {|boatingtest|self.first_name == boatingtest.student}
+        total = total_tests.length.to_f
+        passed = BoatingTest.all.select {|boatingtest| self.first_name == boatingtest.student && boatingtest.test_status == "passed"}
+        passed_t = passed.length.to_f
+        percent = (passed_t / total)*100
+    end
+
+    def self.all
+        @@all
+    end 
+
+    def self.find_student(name)
+        #will take in a first name and output the student (Object) with that name
+        self.all.find {|student| student.firat_name == name}
+    end 
+
 end
