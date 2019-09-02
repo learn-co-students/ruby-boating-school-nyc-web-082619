3e2bf8209f9deab6b33a9f456b2610b2b1de2d61
github username <github email address>
2019-09-01 22:32:24 -0400

    Done.

--- app/models/instructor.rb
+++ app/models/instructor.rb
@@ -1,3 +1,30 @@
 class Instructor
 
+    attr_accessor :name
+
+    def initialize(name)
+        @name = name
+    end
+
+    def pass_student(student_inst, name_of_test)
+        real = BoatingTest.all.select{ |test| test.student == student_inst && test.test_name == name_of_test}
+        
+        if real.empty?
+            BoatingTest.new(student_inst, name_of_test, "passed", self) 
+        else
+            real[0].test_status = "passed"
+            #binding.pry
+        end
+    end
+
+    def fail_student(student_inst, name_of_test)
+        real = BoatingTest.all.select{ |test| test.student == student_inst && test.test_name == name_of_test}
+        
+        if real.empty?
+            BoatingTest.new(student_inst, name_of_test, "failed", self) 
+        else
+            real[0].test_status = "failed"
+        end
+    end
+    
 end
--- tools/console.rb
+++ tools/console.rb
@@ -8,6 +8,30 @@ end
 # This is a convenient place to define variables and/or set up new object instances,
 # so they will be available to test and play around with in your console
 
+spongebob = Student.new("Spongebob")
+patrick = Student.new("Patrick")
+ 
+puff = Instructor.new("Ms.Puff")
+krabs = Instructor.new("Mr.Krabs")
+ 
+no_crashing = spongebob.add_boating_test("Don't Crash 101", "pending", puff)
+power_steering_failure = patrick.add_boating_test("Power Steering 202", "failed", puff)
+power_steering_pass = patrick.add_boating_test("Power Steering 201", "passed", krabs)
+
+power_steering_pass = spongebob.add_boating_test("Power Steering 201", "passed", krabs)
+power_steering_pass = spongebob.add_boating_test("Don't die 101", "passed", krabs)
+power_steering_pass = spongebob.add_boating_test("Don't die 201", "failed", krabs)
+power_steering_pass = patrick.add_boating_test("Don't die 101", "passed", krabs)
+power_steering_pass = patrick.add_boating_test("Power Steering 201", "passed", krabs)
+power_steering_pass = patrick.add_boating_test("Power Steering 201", "passed", krabs)
+
+Student.find_student("Spongebob")
+
+puff.pass_student(spongebob, "Don't Crash 101")
+puff.pass_student(patrick, "Don't Crash 101")
+
+spongebob.grade_percentage
+
 binding.pry
 0 #leave this here to ensure binding.pry isn't the last line
 
