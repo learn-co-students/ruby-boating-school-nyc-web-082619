bb71e871f68d80b3f3863cc1a7b25a8d46071674
Anya <anya.s.litvinova@gmail.com>
2019-09-24 15:20:23 -0400

    Done.

--- app/models/student.rb
+++ app/models/student.rb
@@ -1,3 +1,32 @@
 class Student
+    attr_accessor :first_name
+
+    @@all = []
+
+    def initialize(first_name)
+        @first_name = first_name
+        @@all << self
+    end
+
+    def add_boating_test(name, instructor, status)
+        BoatingTest.new(name, self, instructor, status)
+    end 
+
+    def self.all
+        @@all
+    end
+
+    def self.find_student(first_name)
+        self.all.select{|student|student.first_name == first_name}
+    end
+
+    def grade_percentage
+        studentTests= BoatingTest.all.find_all{|test| test.student.first_name == self.first_name}
+        totalTests= studentTests.length.to_f
+        totalPassed= studentTests.find_all{|test| test.status == "passed"}
+        numPassed= totalPassed.length.to_f
+        percent= (numPassed/totalTests)*100
+    end
+
 
 end
--- tools/console.rb
+++ tools/console.rb
@@ -4,6 +4,18 @@ def reload
   load 'config/environment.rb'
 end
 
+s1 = Student.new("Alan")
+s2 = Student.new("Bob")
+s3 = Student.new("Jenn")
+
+i1 = Instructor.new("Instructor Jeff")
+i2 = Instructor.new("Instructor Sean")
+i3 = Instructor.new("Instructor Glenn")
+
+bt1 = BoatingTest.new("test 1", s1, i1, "passed")
+bt2 = BoatingTest.new("test 2", s2, i2, "failes")
+bt3 = BoatingTest.new("test 3", s3, i3, "passed")
+
 # Insert code here to run before hitting the binding.pry
 # This is a convenient place to define variables and/or set up new object instances,
 # so they will be available to test and play around with in your console
